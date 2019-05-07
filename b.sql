4d2fb81822af6fc9ea0cd5c3e9fa542d1a57adfe
Thanuchiya <thanuchiya@gmail.com>
2019-05-07 16:16:33 +0200

    b.sql

--- b.sql
+++ b.sql
@@ -1,15 +1,5 @@
-WITH 
- cte (base,nom)
-AS (
-SELECT table_name
-FROM information_schema.tables
-WHERE table_type = 'BASE TABLE'
-AND table_schema NOT IN ('pg_catalog', 'information_schema')
-	
-UNION ALL
-SELECT datname FROM pg_database
-)
-SELECT * FROM cte GROUP BY base,nom;
-
-
-
+SELECT * 
+FROM information_schema.tables 
+WHERE table_type = 'BASE TABLE' 
+    AND table_schema = 'public' 
+ORDER BY table_type, table_name
